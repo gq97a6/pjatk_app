874146589406d36becb2c557a3f58072dcd968c1
gq97a6 <sz.kg@protonmail.com>
2023-02-02 12:34:53 +0100

    Initial commit.

---
+++ app/src/main/java/gq97a6/pjatk/app/MainActivity.kt
@@ -0,0 +1,22 @@
+package gq97a6.pjatk.app
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import gq97a6.pjatk.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var b: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+    }
+}
